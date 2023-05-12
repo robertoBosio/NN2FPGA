af977b2bc766eba9533e4aa4653dd367b69d946e
minnellf <filippo.minnella@polito.it>
2023-05-12 16:44:02 +0000

    standard name

--- tcl/bd_design.tcl
+++ tcl/bd_design.tcl
@@ -692,10 +692,7 @@ Port;FD4A0000;FD4AFFFF;1|FPD;DPDMA;FD4C0000;FD4CFFFF;1|FPD;DDR_XMPU5_CFG;FD05000
 
 
   # Create interface connections
-  # connect_bd_intf_net -intf_net Network_0_o_net_conv_30 [get_bd_intf_pins Network_0/o_net_conv_30] [get_bd_intf_pins axi_dma_0/S_AXIS_S2MM]
-  # connect_bd_intf_net -intf_net Network_0_o_net_conv_3 [get_bd_intf_pins Network_0/o_net_conv_3] [get_bd_intf_pins axi_dma_0/S_AXIS_S2MM]
-  # connect_bd_intf_net -intf_net Network_0_o_net_conv_21 [get_bd_intf_pins Network_0/o_net_conv_21] [get_bd_intf_pins axi_dma_0/S_AXIS_S2MM]
-  connect_bd_intf_net -intf_net Network_0_o_net_conv_12 [get_bd_intf_pins Network_0/o_net_conv_12] [get_bd_intf_pins axi_dma_0/S_AXIS_S2MM]
+  connect_bd_intf_net -intf_net Network_0_o_net_conv_30 [get_bd_intf_pins Network_0/o_outp1] [get_bd_intf_pins axi_dma_0/S_AXIS_S2MM]
   connect_bd_intf_net -intf_net axi_dma_0_M_AXIS_MM2S [get_bd_intf_pins Network_0/i_inp_1] [get_bd_intf_pins axi_dma_0/M_AXIS_MM2S]
   connect_bd_intf_net -intf_net axi_dma_0_M_AXI_MM2S [get_bd_intf_pins axi_dma_0/M_AXI_MM2S] [get_bd_intf_pins zynq_ultra_ps_e_0/S_AXI_HP0_FPD]
   connect_bd_intf_net -intf_net axi_dma_0_M_AXI_S2MM [get_bd_intf_pins axi_dma_0/M_AXI_S2MM] [get_bd_intf_pins zynq_ultra_ps_e_0/S_AXI_HP1_FPD]
