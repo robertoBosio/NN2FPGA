d0c6d9e83c09f0b12547a31ae07b60916152a777
fabrizio-ottati <fabriziottati@gmail.com>
2023-05-13 12:48:03 +0200

    Settings for the Makefile, such as variables and so on.

---
+++ nn2fpga/settings.mk
@@ -0,0 +1,18 @@
+export MEM := MemoryManagement
+export NAME := Network
+export CC := gcc
+export CXX := g++
+export VITIS_HLS_ROOT := /eda/xilinx/Vitis_HLS/2022.2/
+export BOARD_PATH := \
+	/home/filippo/.Xilinx/Vivado/2022.2/xhub/board_store/xilinx_board_store
+export DEPS := $(NAME).hpp
+export COSIM := 0
+export NN2FPGA_CC_ROOT := $(shell pwd)/cc
+export NN2FPGA_ROOT := $(shell pwd)
+
+export CXXFLAGS := -I$(VITIS_HLS_ROOT)/include
+export PRJ_ROOT := $(shell cd .. && pwd)/nn2fpga-work
+export TOP_NAME := Network
+# export BOARD=ULTRA96v2
+# export ONNX_PATH=./onnx/Brevonnx_resnet_final_fx.onnx
+
