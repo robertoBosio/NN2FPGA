dbda061bb2fa185b750c19a151c2922f398f4fef
fabrizio-ottati <fabriziottati@gmail.com>
2023-05-13 15:21:21 +0200

    Fixed typos here and there: still need to test the compile recipe.

--- nn2fpga/settings.mk
+++ nn2fpga/settings.mk
@@ -10,7 +10,7 @@ export COSIM := 0
 export NN2FPGA_CC_ROOT := $(shell pwd)/cc
 export NN2FPGA_ROOT := $(shell pwd)
 
-export CXXFLAGS := -I$(VITIS_HLS_ROOT)/include
+export CXXFLAGS := -I$(VITIS_HLS_ROOT)/include -I$(NN2FPGA_CC_ROOT)/include
 export PRJ_ROOT := $(shell cd .. && pwd)/work
 export TOP_NAME := Network
 # export BOARD=ULTRA96v2
