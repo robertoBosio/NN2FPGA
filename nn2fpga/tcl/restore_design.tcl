a426e3be58fe1e096c51389e53f03ad0960070c9
fabrizio-ottati <fabriziottati@gmail.com>
2023-05-13 12:52:51 +0200

    Adapted TCL to new directory structure and programmable results directory.

--- nn2fpga/tcl/restore_design.tcl
+++ nn2fpga/tcl/restore_design.tcl
@@ -1,8 +1,8 @@
+set NN2FPGA_ROOT [lindex $argv 2]
+source "${NN2FPGA_ROOT}/tcl/settings.tcl"
+
 set impl_sel "solution_0"
 
-set BOARD $::env(BOARD)
-
-set TOP_NAME Network
 set PRJ_NAME ${TOP_NAME}${BOARD}
 
 open_project ${PRJ_NAME}_ip
