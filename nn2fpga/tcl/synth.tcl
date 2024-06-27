16ca4faac1f806ee4b6cad81f18c6bd5709bc8b8
Teour <teodoro.97@hotmail.it>
2024-06-27 15:58:26 +0000

    remove silvia

--- nn2fpga/tcl/synth.tcl
+++ nn2fpga/tcl/synth.tcl
@@ -88,7 +88,7 @@ config_interface -m_axi_latency 1
 # MOD done to reduce LUT usage with a small performance degradation
 config_compile -pipeline_style stp -enable_auto_rewind=false
 
-csynth_design
+synth_design
 
 export_design -flow syn
 #export_design
