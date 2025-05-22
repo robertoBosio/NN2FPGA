37028183bc77f74db5437954e5fd2943fad86a83
robertoBosio <roberto.bosio98@gmail.com>
2025-05-22 15:30:40 +0000

    chore: Added SILVIA compilation inside Dockerfile

--- run.sh
+++ run.sh
@@ -4,11 +4,15 @@ set -e
 # ----- Configuration -----
 IMAGE_NAME="nn2fpga-container-image"
 CONTAINER_NAME="nn2fpga-container"
-WORKSPACE_DIR="$(pwd)"
+WORKSPACE_DIR="$(pwd)/.."
 HISTORY_FILE="${WORKSPACE_DIR}/.docker_bash_history"
 USERNAME="$(whoami)"
 USER_ID="$(id -u)"
 GROUP_ID="$(id -g)"
+XILINX_DIR="/tools/Xilinx"
+XRT_DIR="/opt/xilinx/xrt"
+DATASET_DIR="/home-ssd/datasets"
+WORKSPACE_ROOT_DIR="/workspace"
 
 # ----- Ensure bash history file exists -----
 touch "${HISTORY_FILE}"
@@ -17,6 +21,7 @@ touch "${HISTORY_FILE}"
 if ! docker image inspect ${IMAGE_NAME} > /dev/null 2>&1; then
     echo "Building Docker image '${IMAGE_NAME}'..."
     docker build \
+        --file docker/Dockerfile \
         --build-arg USERNAME="${USERNAME}" \
         --build-arg USER_ID="${USER_ID}" \
         --build-arg GROUP_ID="${GROUP_ID}" \
@@ -26,14 +31,15 @@ fi
 # ----- Run the container -----
 docker run -it --rm \
     --name "${CONTAINER_NAME}" \
-    -v "${WORKSPACE_DIR}:/workspace" \
-    -v "${HISTORY_FILE}:/workspace/.docker_bash_history" \
-    -v /tools:/tools \
-    -v /opt:/opt \
-    -v /home-ssd/datasets:/home-ssd/datasets \
+    -v "${WORKSPACE_DIR}:${WORKSPACE_ROOT_DIR}" \
+    -v "${HISTORY_FILE}:${WORKSPACE_ROOT_DIR}/NN2FPGA/.docker_bash_history" \
+    -v "${XILINX_DIR}:${XILINX_DIR}" \
+    -v "${XRT_DIR}:${XRT_DIR}" \
+    -v "${DATASET_DIR}:/home/datasets" \
     --network=host \
     --gpus all \
     --env NVIDIA_VISIBLE_DEVICES=all \
     --env NVIDIA_DRIVER_CAPABILITIES=all \
+    --env NN2FPGA_ROOT_DIR="${WORKSPACE_ROOT_DIR}/NN2FPGA" \
     "${IMAGE_NAME}" \
     bash
