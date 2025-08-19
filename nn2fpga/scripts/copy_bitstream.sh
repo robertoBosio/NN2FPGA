cfabc9e2e3680d38f656b5bc33fe9df8ccaa29d7
robertoBosio <roberto.bosio98@gmail.com>
2025-08-19 09:36:28 +0000

    chore: Removed unused stuff

--- nn2fpga/scripts/copy_bitstream.sh
+++
@@ -1,2 +0,0 @@
-scp ${PRJ_ROOT}/${prj_name}_example/${prj_name}_example.gen/sources_1/bd/design_1/hw_handoff/design_1.hwh tmp/design_1.hwh
-scp ${PRJ_ROOT}/${prj_name}_example/design_1.bit tmp/design_1.bit
--- nn2fpga/tcl/export2022.tcl
+++
@@ -1,7 +0,0 @@
-# Grep firt element of args list
-set PROJ [lindex $argv 3]
-
-open_project ${PROJ}
-open_solution solution
-export_design
-exit
