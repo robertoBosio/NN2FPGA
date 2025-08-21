8b91af835f0e0a8791f1a6eb327f2aad451cc47f
robertoBosio <roberto.bosio98@gmail.com>
2025-08-21 08:33:22 +0000

    fix: Written patter to substitute Gemm even in case of not Reshape in input

--- nn2fpga/py/backend/transformation/fullyconnected_to_conv.py
+++ nn2fpga/py/backend/transformation/fullyconnected_to_conv.py
@@ -10,6 +10,9 @@ logger = logging.getLogger(__name__)
 def reshape_quant_gemm_pattern(
     op, x, W, B
 ):
+    """
+    Pattern for Reshape -> Quant -> Gemm.
+    """
 
     x2d = op.Reshape(x, _allow_other_inputs=True)
     x2d_quant = op.Quant(
@@ -37,7 +40,26 @@ def flatten_quant_gemm_pattern(
     y = op.Gemm(x2d_quant, W, B, alpha=1.0, beta=1.0, transB=1, _allow_other_attributes=True)
     return y
 
-def repl_conv1x1_from_reshape(op, x, W, B, **kwargs):
+def quant_gemm_pattern(
+    op, x, W, B
+):
+    """
+    Pattern for Quant -> Gemm.
+    """
+    x_quant = op.Quant(
+        x,
+        _domain="qonnx.custom_op.general",
+        _allow_other_attributes=True,
+        _allow_other_inputs=True,
+    )
+    y = op.Gemm(x_quant, W, B, alpha=1.0, beta=1.0, transB=1, _allow_other_attributes=True)
+    return y
+
+def rewrite_conv1x1(op, x, W, B, **kwargs):
+
+    """
+    Rewrite X -> Conv1x1 -> Reshape.
+    """
 
     reshape_weights_shape_tensor = helper.make_tensor(
         name=f"reshape_weights_shape_{x.name}",
@@ -72,6 +94,54 @@ def repl_conv1x1_from_reshape(op, x, W, B, **kwargs):
 
     return y
 
+def rewrite_conv1x1_with_unsqueeze_input(op, x, W, B, **kwargs):
+    """
+    Rewrite X -> Reshape -> Conv1x1 -> Reshape.
+    """
+
+    reshape_input_shape_tensor = helper.make_tensor(
+        name=f"reshape_input_shape_{x.name}",
+        data_type=TensorProto.INT64,
+        dims=[4],
+        vals=[0, 0, 1, 1],
+    )
+    reshape_input = op.Constant(value=reshape_input_shape_tensor)
+
+    x_reshaped = op.Reshape(x, reshape_input)
+
+    reshape_weights_shape_tensor = helper.make_tensor(
+        name=f"reshape_weights_shape_{x.name}",
+        data_type=TensorProto.INT64,
+        dims=[4],
+        vals=[0, 0, 1, 1],
+    )
+    reshape_input = op.Constant(value=reshape_weights_shape_tensor)
+
+    W_reshaped = op.Reshape(W, reshape_input)
+
+    y = op.Conv(
+        x_reshaped,
+        W_reshaped,
+        B,
+        dilations=[1, 1],
+        group=1,
+        kernel_shape=[1, 1],
+        pads=[0, 0, 0, 0],
+        strides=[1, 1],
+    )
+
+    reshape_output_shape_tensor = helper.make_tensor(
+        name=f"reshape_output_shape_{x.name}",
+        data_type=TensorProto.INT64,
+        dims=[2],
+        vals=[1, -1],
+    )
+    reshape_output = op.Constant(value=reshape_output_shape_tensor)
+
+    y = op.Reshape(y, reshape_output)
+
+    return y
+
 
 class FullyConnectedToPointwise(Transformation):
     """
@@ -84,8 +154,9 @@ class FullyConnectedToPointwise(Transformation):
     def __init__(self):
         self._rewrite_rule_set = pattern.RewriteRuleSet(
             [
-                pattern.RewriteRule(reshape_quant_gemm_pattern, repl_conv1x1_from_reshape),
-                pattern.RewriteRule(flatten_quant_gemm_pattern, repl_conv1x1_from_reshape),
+                pattern.RewriteRule(reshape_quant_gemm_pattern, rewrite_conv1x1),
+                pattern.RewriteRule(flatten_quant_gemm_pattern, rewrite_conv1x1),
+                pattern.RewriteRule(quant_gemm_pattern, rewrite_conv1x1_with_unsqueeze_input),
             ],
             commute=True,
         )
